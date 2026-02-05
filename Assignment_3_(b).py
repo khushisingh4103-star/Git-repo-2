0c0f178982d3c710cd9d1a0b0eeac1c447344f58
khushisingh4103-star <khushisingh4103@gmail.com>
2026-02-06 00:53:59 +0530

    Commit Message

---
+++ Assignment_3_(b).py
@@ -0,0 +1,11 @@
+import math
+
+n = float(input("Enter a number: "))
+
+sq_root = math.sqrt(n)
+log_val = math.log(n)
+sine_val = math.sin(n)
+
+print("Square root: ", sq_root)
+print("Logarithm: ", log_val)
+print("Sine: ", sine_val)
